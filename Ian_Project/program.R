b143a0a3116cb23b968b266d16e0e0b625728651
IanPorto <elmerian1106@gmail.com>
2024-07-24 14:52:49 -0400

    updating program.r

--- Ian_Project/program.R
+++ Ian_Project/program.R
@@ -6,7 +6,6 @@ data <- na.omit(original_data)
 
 
 data <- data |> mutate(average_score = (sentimentAnalysis_score+ sentimentr_score+ syuzhet_score)/3)
-print(data[, 13:16])
 # Mean Character Sentiment Score
 filtered_data <- data |>
   group_by(character) |>
@@ -14,7 +13,7 @@ filtered_data <- data |>
   ungroup()
 result <- filtered_data |>
   group_by(character) |>
-  summarize(mean_score = mean(average_score, na.rm = TRUE)) |>
+  summarize(mean_score = mean(average_score)) |>
   filter(mean_score != 0)
 ggplot(result , aes(x = character, y = mean_score)) + geom_bar(stat = "Identity")
 
@@ -26,7 +25,7 @@ episode_data <- data |>
 new_data <- merge(data, episode_data, by = c("season", "episode")) 
 average_episode <- new_data |>
   group_by(episode_index) |>
-  summarise(mean_score = mean(average_score, na.rm = TRUE)) |>
+  summarise(mean_score = mean(average_score)) |>
   left_join(new_data |> select(episode_index, imdb_rating) |> distinct(), by = "episode_index")
 ggplot(average_episode, aes(x = mean_score, y = imdb_rating)) + geom_point() + geom_smooth(method = "lm", se = FALSE) +
   labs(title = paste("Correlation:", cor(average_episode$mean_score, average_episode$imdb_rating), 2))
