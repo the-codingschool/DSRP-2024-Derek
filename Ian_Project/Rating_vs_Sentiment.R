196084912de63357479acdc3b95d566f22a43a60
IanPorto <elmerian1106@gmail.com>
2024-07-24 14:52:17 -0400

    added Mean character sentiment and comparison of rating to sentiment

--- Ian_Project/Rating_vs_Sentiment.R
+++ Ian_Project/Rating_vs_Sentiment.R
@@ -4,21 +4,6 @@ library(ggplot2)
 original_data <- read.csv("data/office_sentiment .csv") 
 data <- na.omit(original_data)
 
-
-data <- data |> mutate(average_score = (sentimentAnalysis_score+ sentimentr_score+ syuzhet_score)/3)
-print(data[, 13:16])
-# Mean Character Sentiment Score
-filtered_data <- data |>
-  group_by(character) |>
-  filter(n() > 40) |>
-  ungroup()
-result <- filtered_data |>
-  group_by(character) |>
-  summarize(mean_score = mean(average_score, na.rm = TRUE)) |>
-  filter(mean_score != 0)
-ggplot(result , aes(x = character, y = mean_score)) + geom_bar(stat = "Identity")
-
-
 #Episode score vs Episode rating
 episode_data <- data |> 
   distinct(season, episode) |>
