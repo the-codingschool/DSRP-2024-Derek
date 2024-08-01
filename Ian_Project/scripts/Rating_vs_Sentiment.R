d95a52d5e64d0c75348947e6ad9587f8158ae4ba
IanPorto <elmerian1106@gmail.com>
2024-08-01 16:51:31 -0400

    updates

--- Ian_Project/scripts/Rating_vs_Sentiment.R
+++ Ian_Project/scripts/Rating_vs_Sentiment.R
@@ -4,10 +4,15 @@ library(ggplot2)
 original_data <- read.csv("data/office_sentiment .csv") 
 data <- na.omit(original_data)
 
-data <- data |> mutate(average_score = (sentimentAnalysis_score*0.5)+ (sentimentr_score*0.3)+ (syuzhet_score*0.2))
+data <- data |> mutate(average_score = sentimentr_score)
+data <- data |>
+  #filter(season == 5) |>
+  group_by(character) |>
+  filter(n() > 45) |>
+  ungroup()
 #Episode score vs Episode rating
 episode_data <- data |> 
-  filter(character=="Dwight") |>
+  filter(character=="Andy") |>
   distinct(season, episode) |>
   mutate(episode_index = row_number())
 new_data <- merge(data, episode_data, by = c("season", "episode")) 
@@ -15,5 +20,6 @@ average_episode <- new_data |>
   group_by(episode_index) |>
   summarise(mean_score = mean(average_score, na.rm = TRUE)) |>
   left_join(new_data |> select(episode_index, imdb_rating) |> distinct(), by = "episode_index")
-ggplot(average_episode, aes(x = mean_score, y = imdb_rating)) + geom_point() + geom_smooth(method = "lm", se = FALSE) +
-  labs(title = paste("Correlation:", cor(average_episode$mean_score, average_episode$imdb_rating), 2))
+ggplot(average_episode, aes(x = mean_score, y = imdb_rating)) + geom_point() + geom_smooth(method = "lm", se = FALSE)
+lm(average_episode$imdb_rating ~ average_episode$mean_score, data = average_episode)
+print(cor.test(average_episode$mean_score, average_episode$imdb_rating, method = "spearman"))
--- Ian_Project/scripts/program.R
+++ Ian_Project/scripts/program.R
@@ -1,15 +1,28 @@
 library(dplyr)
 library(ggplot2)
 
+
+selected_character <- "Kevin"
+
 original_data <- read.csv("data/office_sentiment .csv") 
 data <- na.omit(original_data)
 
 data <- data |> mutate(average_score = (sentimentAnalysis_score*0.5)+ (sentimentr_score*0.3)+ (syuzhet_score*0.2))
-# Mean Character Sentiment Score
-
-result <- data |>
-  filter(character == "Pam")
+#Average character sentiment by season
+average_season <- data |>
+  filter(character == selected_character) |>
   group_by(season) |>
-  summarize(mean_score = mean(average_score, na.rm = TRUE)) |>
-  filter(mean_score != 0)
-ggplot(result , aes(x = character, y = mean_score)) + geom_line()
+  summarise(mean_score = mean(average_score, na.rm = TRUE))
+#Average character sentiment by episode
+episode_data <- data |> 
+  distinct(season, episode) |>
+  mutate(episode_index = row_number())
+new_data <- merge(data, episode_data, by = c("season", "episode")) |> filter(character == selected_character)
+
+average_episode <- new_data |>
+  group_by(episode_index) |>
+  summarise(mean_score = mean(average_score, na.rm = TRUE)) 
+
+ggplot(average_season, aes(x = season, y = mean_score)) + geom_line()
+
+
