2883b686df37dea67483a0bc18422839bedeac28
Matthew Fin <Matthewj@jba4.com>
2024-08-02 12:59:06 -0700

    Updata

--- Matthew_Project/Models.R
+++ Matthew_Project/Models.R
@@ -15,7 +15,7 @@ result <- data %>%
   group_by(character_name, season, episode) %>% 
   filter(sentimentAnalysis_score != 0) %>%
   summarise(mean_value = mean(sentimentAnalysis_score)) %>%
-  filter(season == 9) #Edit this and below to change season
+  filter(season == 3) #Edit this and below to change season
 
 resultr <- data %>%
   mutate(character_name_plus_2 = paste(character_name, "2", sep = ''))
@@ -23,7 +23,7 @@ resultr <- resultr %>%
   group_by(character_name_plus_2, season, episode) %>% 
   filter(sentimentr_score != 0) %>%
   summarise(mean_value = mean(sentimentr_score)) %>%
-  filter(season == 9) #Edit this too
+  filter(season == 3) #Edit this too
 
 resultz <- data %>%
   mutate(character_name_plus_3 = paste(character_name, "3", sep = ''))
@@ -31,7 +31,7 @@ resultz <- resultz %>%
   group_by(character_name_plus_3, season, episode) %>% 
   filter(syuzhet_score != 0) %>%
   summarise(mean_value = mean(syuzhet_score)) %>%
-  filter(season == 9) #Edit this too
+  filter(season == 3) #Edit this too
   
 df_wide <- result %>%
   pivot_wider(
@@ -46,6 +46,8 @@ df_wider <- resultr %>%
     values_from = mean_value,         
     names_sort = TRUE) %>%                 
   arrange(episode, season)
+
+colnames(df_wide)
 
 df_widez <- resultz %>%
   pivot_wider(
@@ -62,7 +64,7 @@ print(merged_df)
   
 results <- data %>%
   group_by(season) %>%
-  filter(season == 9) ##Change this
+  filter(season == 3) ##Change this
 
 new_data <- results %>%
   group_by(episode, imdb_rating) %>%
@@ -71,18 +73,22 @@ new_data <- results %>%
 merged_df$imdb_rating <- paste(new_data$imdb_rating)
 merged_df$imdb_rating <- as.numeric(merged_df$imdb_rating)
 
+print(merged_df, n= 26)
+col_v <- merged_df[,44]
+print(col_v, n =25)
+print(merged_df, n = 25)
 
 set.seed(42)
 datas <- merged_df[, c("imdb_rating", "Jim", "Jim2", "Jim3")]
 split <- sample.split(datas$imdb_rating, SplitRatio = 0.8)
 train_data <- subset(datas, split == TRUE)
 test_data <- subset(datas, split == FALSE)
-lr_model <- lm(imdb_rating ~ Jim + Jim2 + Jim3, data = train_data)
+lr_model <- lm(imdb_rating ~ Jim + Jim2 + Jim3 , data = train_data)
 pred <- predict(lr_model, newdata = test_data)
 ggplot(test_data, aes(x = imdb_rating, y = pred)) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "red") +
-  labs(title = "Real v.s. Predicted Values Season 9",
+  labs(title = "Real v.s. Predicted Values Season 1",
        x = "True IMDB Rating",
        y = "Predicted IMDB Rating",
        ) +
@@ -92,7 +98,6 @@ ggplot(test_data, aes(x = imdb_rating, y = pred)) +
   scale_y_continuous(
     limits = c(6,10)
   )
-r_squared <- summary(lr_model)$r.squared
-r_squared
+summary(lr_model)
 
 
--- Matthew_Project/Visualizations.R
+++ Matthew_Project/Visualizations.R
@@ -2,52 +2,92 @@ library(dplyr)
 library(ggplot2)
 library(caTools)
 library(readr)
+library(tidyverse)
+
 
 # read in dataset
 data <- read.csv("data/office_sentiment .csv")
 data <- data |>
   rename('character_name' = character)
-head(data, n = 20)
+head(data, n = 5)
 
 
 summary_data <- data %>%
-  group_by(season, character_name) %>%
-  summarise(total_appearances = n(), 
-            sum_score = sum(sentimentr_score)) %>%
-  filter(total_appearances > 50)
+  group_by(character_name) %>%
+  summarise(total_appearances = n()) %>%
+  arrange(desc(total_appearances))
+summary_data
 
+##Main Characters Michael, Dwight, Jim, Pam, Andy
 
-ggplot(summary_data, aes(x = season, y = sum_score , fill = character_name)) + 
+ggplot(summary_data, aes(x = season, y = mean_score , fill = character_name)) + 
   geom_bar(stat = "identity", color = "skyblue", width = 0.7) +
-  labs(title = "Season 1 Sum SentimentR Score by Character/Season", 
+  labs(title = "Mean SentimentR Score by Character/Season", 
        x = "Character",
-       y = "Sum Score") +
+       y = "Mean Season Score") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 ##Research Question Idea -> Relationship between character emotions and imdb rating, perhpas, average emotion throughout episode, or relationship between one character's emotions and rating
 
-data$dwightSentiment <- ifelse(data$character == "Dwight", data$sentimentAnalysis_score, NA)
-data$dwightSentiment2 <- ifelse(data$character == "Dwight", data$sentimentr_score, NA)
-data$dwightSentiment3 <- ifelse(data$character == "Dwight", data$syuzhet_score, NA)
+
+
+
+data$jimSentiment <- ifelse(data$character == "Jim", data$sentimentAnalysis_score, NA)
+data$jimSentiment2 <- ifelse(data$character == "Jim", data$sentimentr_score, NA)
+data$jimSentiment3 <- ifelse(data$character == "Jim", data$syuzhet_score, NA)
 
 set.seed(42)
-datas <- data[, c("imdb_rating", "dwightSentiment", "dwightSentiment2", "dwightSentiment3")]
+datas <- data[, c("imdb_rating", "jimSentiment", "jimSentiment2", "jimSentiment3", "dwightSentiment", "dwightSentiment2", "dwightSentiment3", "season")]
 split <- sample.split(datas$imdb_rating, SplitRatio = 0.8)
 train_data <- subset(datas, split == TRUE)
 test_data <- subset(datas, split == FALSE)
-lr_model <- lm(imdb_rating ~ dwightSentiment + dwightSentiment2 + dwightSentiment3, data = train_data)
+lr_model <- lm(imdb_rating ~ jimSentiment + jimSentiment2 + jimSentiment3 + dwightSentiment + dwightSentiment2 + dwightSentiment3, data = train_data)
 pred <- predict(lr_model, newdata = test_data)
-ggplot(test_data, aes(x = imdb_rating, y = pred)) +
+ggplot(test_data, aes(x = pred, y = imdb_rating, color = factor(season))) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "red") +
   labs(title = "Real v.s. Predicted Values",
-       x = "True IMDB Rating",
-       y = "Predicted IMDB Rating")
+       x = "Predicted IMDB Rating",
+       y = "True IMDB Rating",
+       color = "Season")
 r_squared <- summary(lr_model)$r.squared
 r_squared
 
+
+
+
 summary(data)
 
-ggplot(data, aes(y = imdb_rating, color = factor(season))) +
+sdata <- data %>%
+  group_by(character_name, season) %>%
+  filter(character_name == "Dwight") %>%
+  filter(sentimentr_score != 0) 
+
+ggplot(sdata, aes(y = sentimentr_score, color = factor(season))) +
   geom_boxplot() +
   labs(title = "Box Plot of IMDB Rating")
+
+
+ggplot(sdata, aes(y = sentimentr_score, color = factor(episode))) +
+  geom_boxplot() +
+  labs(title = "Box Plot of IMDB Rating")
+
+ggplot(sdata, aes(y = mean_score, x = factor(season), group = 1)) +
+  geom_point(color = "blue") +
+  geom_line(color = "red", linetype = "dashed") +
+  labs(title = "Mean SentimentR Score of Jim Over Time",
+       x = "Season",
+       y = "Mean SentimentR Score")
+
+ndata <- data%>%
+  group_by(season, ) %>%
+  summarise(mean_scores = mean(imdb_rating, na.rm = TRUE))
+
+ggplot(ndata, aes(y = mean_scores, x= factor(season), group = 1)) +
+  geom_point(color = "blue") +
+  geom_line(color = "red", linetype = "dashed") +
+  labs(title = "Mean IMDB Rating Over Time",
+       x = "Season",
+       y = "Mean IMDB Rating")
+
+
